e882be7abdafcd76d6ebc9d6f5800bb9269c8fb1
K Subramanyeshwara <subramanyeshwarak@gmail.com>
2024-06-18 23:15:22 +0530

    tried 2 or 3 extra logics

--- 03 Counter/script.js
+++ 03 Counter/script.js
@@ -1,4 +1,5 @@
 const count = document.querySelector("[data-count]");
+const countOne = document.querySelector("#count");
 const plus = document.querySelector(".fa-plus");
 const minus = document.querySelector(".fa-minus");
 const reset = document.querySelector("#reset");
@@ -12,8 +13,10 @@ jump.addEventListener("change", (e) => {
 });
 
 plus.addEventListener("click", () => {
-  let counts = parseInt(count.dataset.count);
-  count.textContent = count.dataset.count = counts + parseInt(jump.value);
+  // let counts = parseInt(count.dataset.count);
+  let counts = parseInt(countOne.innerText);
+  const inputValue = jump.value;
+  count.textContent = count.dataset.count = counts + parseInt(inputValue);
 });
 minus.addEventListener("click", () => {
   let counts = parseInt(count.dataset.count);
@@ -23,4 +26,5 @@ minus.addEventListener("click", () => {
 reset.addEventListener("click", () => {
   count.textContent = "0";
   count.dataset.count = "0";
+  jump.value = "1";
 });
